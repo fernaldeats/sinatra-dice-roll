b03a9ff8f599a439c8ecf20f0a49942f5d3afffe
fernaldeats <42142633+fernaldeats@users.noreply.github.com>
2023-10-18 00:35:59 +0000

    added erbs for each html page

--- dice.rb
+++ dice.rb
@@ -12,31 +12,8 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "<html>
-  <head>
-  </head>
-    <body>
-      <h1> Dice Roll</h1>
-        <p>
-          <ul>
-              <li>
-                <a href=\"/dice/2/6\">Roll two 6-sided dice</a>
-              </li>
-              <li>
-                <a href=\"/dice/2/10\">Roll two 10-sided dice</a>
-                </li>
-              <li>
-                <a href=\"/dice/1/20\">Roll one 20-sided die</a>
-                </li>
-              <li>
-                <a href=\"/dice/5/4\">Roll five 4-sided die</a>
-                </li>
-            </ul>
-        </p>
-        </body>
-  </html>"
+  erb(:elephant)
 end
-
 
 # What happened BTS?
 # GET / HTTP/1.1
@@ -55,10 +32,9 @@ first_die = rand(1..6)
 second_die = rand(1..6)
 sum = first_die + second_die
 
-outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-"<h1>2d6</h1>
-<p>#{outcome}</p>"
+erb(:two_six)
 end
 
 get("/dice/2/10") do
@@ -66,19 +42,18 @@ get("/dice/2/10") do
   second_die = rand(1..10)
   sum = first_die + second_die
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  "<h1>2d10</h1>
-  <p>#{outcome}</p>"
+  erb(:two_ten)
 end
 
 get("/dice/1/20") do
   first_die = rand(1..20)
 
-  outcome = "You rolled a #{first_die}."
+  @outcome = "You rolled a #{first_die}."
 
-  "<h1>1d20</h1>
-  <p>#{outcome}</p>"
+erb(:one_twenty)
+
 end
 
 get("/dice/5/4") do
@@ -89,8 +64,8 @@ get("/dice/5/4") do
   e = rand(1..4)
   sum = a + b + c + d + e
 
-  outcome = "You rolled a #{a}, #{b}, #{c}, #{d}, and #{e} for a total of #{sum}."
+  @outcome = "You rolled a #{a}, #{b}, #{c}, #{d}, and #{e} for a total of #{sum}."
 
-  "<h1>5d4</h1>
-  <p>#{outcome}</p>"
+  erb(:five_four)
+
 end
